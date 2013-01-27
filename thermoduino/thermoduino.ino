244d0eae257e01f2c9092738bf0add45fdedcea8
John Ericksen <johncarl81@gmail.com>
2013-01-26 21:33:24 -0700

    Added tweeting

--- thermoduino/thermoduino.ino
+++ thermoduino/thermoduino.ino
@@ -1,19 +1,33 @@
 #include <SoftwareSerial.h>
-
+#if defined(ARDUINO) && ARDUINO > 18
+#include <SPI.h>
+#endif
+#include <Twitter.h>        //load twitter library see http://www.arduino.cc/playground/Code/TwitterLibrary
+#include <Ethernet.h>
 #include "DHT.h"
-#define DHTPIN 4     // what pin we're connected to
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
 
-int deviceId = 2;
+#define DHTPIN 4     // what pin we're connected to
+#define DHTTYPE DHT11  // DHT 22  (AM2302)
+
+// Ethernet Shield Settings
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte ip[] = { 192, 168, 1, 121 };
+
+int deviceId = 3;
 int loopVal = 0;
+int twitterLoopVal = 0;
+boolean enable_twitter = true;
 
 SoftwareSerial xbeeSerial(2,3);
 DHT dht(DHTPIN, DHTTYPE);
+
+Twitter twitter("TODO");
 
 void setup()
 {
   Serial.begin(9600);
   xbeeSerial.begin(9600);
+  Ethernet.begin(mac, ip);
   
   dht.begin();
 }
@@ -21,12 +35,13 @@ void setup()
 void loop()
 {
   loopVal++;
+  twitterLoopVal++;
   
   if(loopVal == 100){
     float h = dht.readHumidity();
     float t = dht.readTemperature();
     
-    String reading = buildReading(deviceId, h, (t * 9/5 + 32));
+    String reading = buildReading(deviceId, "R", h, (t * 9/5 + 32));
     xbeeSerial.print(reading);
     outputValues(reading);
     loopVal = 0;
@@ -48,46 +63,74 @@ void loop()
     if(c == 'e'){
       serialRead = false;
       outputValues(content);
-      parseValues(content);
+      xbeeSerial.print(content);
     }
   }
   
-  delay(10000);
+  delay(100);
 }
 
 void outputValues(String content){
-  /*Serial.print("Validation: ");
-      Serial.print(validate(content));
-      Serial.print(" Content: ");
-      Serial.println(content);*/
-      parseValues(content);
-}
-
-void parseValues(String content){
  
   int firstComma = content.indexOf(",");
   int secondComma = content.indexOf(",", firstComma+1);
   int thirdComma = content.indexOf(",", secondComma+1);
+  int forthComma = content.indexOf(",", thirdComma+1);
   
   int id = content.substring(1,firstComma).toInt();
-  float temperature = stringToFloat(content.substring(firstComma+1, secondComma));
-  float humidity = stringToFloat(content.substring(secondComma+1, thirdComma));
+  String type = content.substring(firstComma+1, secondComma);
+  float temperature = stringToFloat(content.substring(secondComma+1, thirdComma));
+  float humidity = stringToFloat(content.substring(thirdComma+1, forthComma));
   
   Serial.print("Id: ");
   Serial.print(id);
+  Serial.print(" Type: ");
+  Serial.print(type);
   Serial.print(" H: ");
   Serial.print(humidity);
   Serial.print(" T: ");
   Serial.print(temperature);
   Serial.print(" V: ");
-  Serial.println(validate(content));
+  Serial.print(validate(content));
+  Serial.print(" Time: ");
+  Serial.println(twitterLoopVal);
+  
+  if(twitterLoopVal > 1200000 && validate(content)){
+    twitterLoopVal = 0;
+    Serial.println("TWEET!!");
+    String message = "The current temperature is " + content.substring(secondComma+1, thirdComma) + "F from the office.";
+    
+    Serial.println(message);
+    char messageBuff[message.length()];
+    message.toCharArray(messageBuff, message.length());
+    
+    if (enable_twitter) {
+     if(twitter.post(messageBuff)) {
+      // Specify &Serial to output received response to Serial.
+      // If no output is required, you can just omit the argument, e.g.
+      // int status = twitter.wait();
+      int status = twitter.wait(&Serial);
+      if (status == 200) {
+        Serial.println("OK.");
+      } else {
+        Serial.print("failed : code ");
+        Serial.println(status);
+      }
+    } else {
+      Serial.println("connection failed.");
+    }
+    }
+    
+  }
 }
 
-String buildReading(int id, float humidity, float temperature){
+String buildReading(int id, String type, float humidity, float temperature){
   
  String reading = "s"; //start
 
  reading.concat(id);
+ reading.concat(",");
+ reading.concat(type);
  reading.concat(",");
  reading.concat(floatToString(temperature));
  reading.concat(",");
@@ -111,7 +154,7 @@ boolean validate(String reading){
   }
   int commaCount = 0;
   int checksum = 0;
-  for(int i = 0; i < reading.length() && commaCount < 3; i++){
+  for(int i = 0; i < reading.length() && commaCount < 4; i++){
     if(reading[i] == ','){
       commaCount++;
     }
